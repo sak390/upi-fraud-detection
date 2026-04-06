e2c9771413a1dab2ef04de7ab94c0ace04d17617
sak390 <shakshipatel383@gmail.com>
2026-04-06 20:41:15 +0530

    Create database.js

---
+++ src/config/database.js
@@ -0,0 +1,35 @@
+const sqlite3 = require('sqlite3').verbose();
+const path = require('path');
+
+const dbPath = path.join(__dirname, '../../database.sqlite');
+
+const db = new sqlite3.Database(dbPath);
+
+db.runAsync = (sql, params = []) => {
+    return new Promise((resolve, reject) => {
+        db.run(sql, params, function(err) {
+            if (err) reject(err);
+            else resolve({ lastID: this.lastID });
+        });
+    });
+};
+
+db.getAsync = (sql, params = []) => {
+    return new Promise((resolve, reject) => {
+        db.get(sql, params, (err, result) => {
+            if (err) reject(err);
+            else resolve(result);
+        });
+    });
+};
+
+db.allAsync = (sql, params = []) => {
+    return new Promise((resolve, reject) => {
+        db.all(sql, params, (err, rows) => {
+            if (err) reject(err);
+            else resolve(rows);
+        });
+    });
+};
+
+module.exports = db;
